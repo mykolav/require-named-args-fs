3c6a2db35bcc7984be5d7fd35620d233c1a2fa05
mykola <naderevnudeduske@gmail.com>
2021-04-18 03:41:39 +0300

    Remove unused code

--- RequireNamedArgs/Support/CSharpAdapters.fs
+++ RequireNamedArgs/Support/CSharpAdapters.fs
@@ -2,17 +2,8 @@
 
 
 module CSharpAdapters =
-
-
     let asOptional<'Derived when 'Derived : null> (baseObj: obj) = 
         match baseObj with 
         | :? 'Derived as derivedObj -> Some derivedObj
         | _                         -> None
-
-
-    type Option<'a> with
-        static member ofType<'Derived when 'Derived : null> (baseObj: obj) = 
-            match baseObj with 
-            | :? 'Derived as derivedObj -> Some derivedObj
-            | _                         -> None
 
